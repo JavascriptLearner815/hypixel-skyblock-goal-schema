f6240849788dc7abd702e783b97ad1726bc99c15
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-02-27 18:35:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const defaultToggle = document.getElementById("default-toggle")
 const defaultSchema = document.getElementById("default-schema")
 const pumpkinMinionToggle = document.getElementById("pumpkin-minion-toggle")
 const pumpkinMinionSchema = document.getElementById("pumpkin-minion-schema")
+const gettingStartedToggle = document.getElementById("getting-started-toggle")
+const gettingStartedSchema = document.getElementById("getting-started-schema")
 
 defaultToggle.addEventListener("click", () => {
   pumpkinMinionSchema.style.display = "none"
+  gettingStartedSchema.style.display = "none"
   defaultSchema.style.display = "block"
 })
 
 pumpkinMinionToggle.addEventListener("click", () => {
   defaultSchema.style.display = "none"
+  gettingStartedSchema.style.display = "none"
   pumpkinMinionSchema.style.display = "block"
+})
+
+gettingStartedToggle.addEventListener("click", () => {
+  defaultSchema.style.display = "none"
+  pumpkinMinionSchema.style.display = "none"
+  gettingStartedSchema.style.display = "block"
 })
