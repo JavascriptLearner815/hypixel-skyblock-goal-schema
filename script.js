d42ebed8d52428433df2ba9ac3eb954560a9e9b1
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-03-01 17:14:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const pumpkinMinionToggle = document.getElementById("pumpkin-minion-toggle")
 const pumpkinMinionSchema = document.getElementById("pumpkin-minion-schema")
 const gettingStartedToggle = document.getElementById("getting-started-toggle")
 const gettingStartedSchema = document.getElementById("getting-started-schema")
+const jotterTextarea = document.getElementById("jotter")
+const jotButton = document.getElementById("jot")
+const jotterSave = localStorage.getItem("jotter")
+
+if (jotterSave) {
+  jotterTextarea.value = jotterSave
+}
 
 defaultToggle.addEventListener("click", () => {
   pumpkinMinionSchema.style.display = "none"
@@ -22,3 +29,9 @@ gettingStartedToggle.addEventListener("click", () => {
   pumpkinMinionSchema.style.display = "none"
   gettingStartedSchema.style.display = "block"
 })
+
+jotButton.addEventListener("click", () => {
+  localStorage.setItem("jotter", jotterTextarea.value)
+  alert("Jotter has been jotted!")
+  console.log("Jotter was successfully jotted.")
+}, false)
