107f48cf8d4468bae3149bd26768210a6bbeaa10
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-03-07 17:03:46 -0600

    Create UpdateAriaLabel Helper Class

---
+++ classes/update-aria-label.js
@@ -0,0 +1,22 @@
+import ParsedDay from "./parsed-day.js"
+import ParsedMonth from "./parsed-month.js"
+
+export default class UpdateAriaLabel {
+  constructor(month, day, year, content) {
+    /**
+     * see comments in ./update.js (Update) class-file
+     * helper classes: UpdateAriaLabel (this), UpdateInnerHTML (./update-inner-html.js)
+    **/
+    this.month = month
+    this.day = day
+    this.year = year
+    this.content = content
+    this.parsedYear = this.year
+    this.parsedContent = this.content
+    const parsedDay = new ParsedDay(this.day)
+    this.parsedDay = parsedDay.parsed
+    const parsedMonth = new ParsedMonth(this.month)
+    this.parsedMonth = parsedMonth.parsed
+    this.parsed = `On the ${this.parsedDay} of ${this.parsedMonth} in ${this.parsedYear}: ${this.parsedContent}`
+  }
+}
